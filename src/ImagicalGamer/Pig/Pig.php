ca1fdba801b7dcfb87ec5e4a00e3f01c8bb61803
Jake <jakehcasto@aol.com>
2016-04-21 15:00:49 -0400

    Cleaning up some

--- src/ImagicalGamer/Pig/Pig.php
+++ src/ImagicalGamer/Pig/Pig.php
@@ -15,23 +15,10 @@ class Pig extends PluginBase implements Listener{
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getLogger()->info(C::GREEN . "Enabled!");
   }
-  public function getInfo(){
-    $info = (C::GREEN . C::BOLD . "Info" . C::RESET . C::GREEN . "\n/pig install <name>\n/pig search <name>");
-    return $info;
-  }
-  public function getFile(){
-    $file = ($this->getServer()->getDataPath() . "plugins/");
-    return $file;
-  }
     public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
         if($cmd->getName() == "pig"){
-          if(empty($args)){
-          $info = $this->plugin->getInfo();
-          $s->sendMessage($info);
-          }
-           if($args[1] === "install"){
-             $file = $this->plugin->getFile();
-             copy("http://8.26.94.171/ImagicalGamer/" . $args[1] . ".phar", $file);
+           if($args[0] === "install"){
+             copy("http://8.26.94.171/ImagicalGamer/" . $args[1] . ".phar", $this->getServer()->getDataPath() . "plugins/" . $args[1] . ".phar");
            }
 }
 }
