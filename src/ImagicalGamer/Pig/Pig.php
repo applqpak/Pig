b50656306d200059f58ca01364b767e31f2a6faa
Jake <jakehcasto@aol.com>
2016-04-21 15:12:24 -0400

    Update Pig.php

--- src/ImagicalGamer/Pig/Pig.php
+++ src/ImagicalGamer/Pig/Pig.php
@@ -15,11 +15,18 @@ class Pig extends PluginBase implements Listener{
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getLogger()->info(C::GREEN . "Enabled!");
   }
-    public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
+  public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
         if($cmd->getName() == "pig"){
+           $s->sendMessage(C::GREEN . C::BOLD . "Pig-Help!" . C::RESET . C::GREEN . "\nInstall: /pig install <PluginName>\nRemove: /pig remove <PluginName>")
            if($args[0] === "install"){
-             copy("http://8.26.94.171/ImagicalGamer/" . $args[1] . ".phar", $this->getServer()->getDataPath() . "plugins/" . $args[1] . ".phar");
+             copy("http://8.26.94.171/ImagicalGamer/" . $args[1], $this->getServer()->getDataPath() . "plugins/" . $args[1] . ".phar");
+             $s->sendMessage(C::GREEN . $args[1] . " Installed Successfully!");
            }
-}
-}
-}
+           if($args[0] === "remove"){
+            $plugtodelete = $args[1];
+            $this->plugin->getServer()->getPluginManager()->disablePlugin($args[1]);
+            unlink($this->getServer()->getDataPath() . "plugins/" . $plugtodelete . ".phar");
+           }
+        }
+     }
+   }
