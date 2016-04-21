df24ce7c0abcfe273c325f0f275bb277587c91c9
Jake <jakehcasto@aol.com>
2016-04-21 15:17:35 -0400

    Update Pig.php

--- src/ImagicalGamer/Pig/Pig.php
+++ src/ImagicalGamer/Pig/Pig.php
@@ -22,10 +22,8 @@ class Pig extends PluginBase implements Listener{
              copy("http://8.26.94.171/ImagicalGamer/" . $args[1], $this->getServer()->getDataPath() . "plugins/" . $args[1] . ".phar");
              $s->sendMessage(C::GREEN . $args[1] . " Installed Successfully!");
            }
-           if($args[0] === "remove"){
-            $plugtodelete = $args[1];
-            $this->plugin->getServer()->getPluginManager()->disablePlugin($args[1]);
-            unlink($this->getServer()->getDataPath() . "plugins/" . $plugtodelete . ".phar");
+           if($args[0] === "remove" or "delete"){
+            unlink($this->getServer()->getDataPath() . "plugins/" . $args[1] . ".phar");
            }
         }
      }
